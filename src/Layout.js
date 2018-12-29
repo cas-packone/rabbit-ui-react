dea8e7fed6506cd67d38c6a71f14e5bbc6a552e7
excelwang <wanghuajin999@163.com>
2018-12-29 10:41:37 +0800

    prepare a stock-style menu component for react-admin layout

--- src/Layout.js
+++ src/Layout.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Layout } from 'react-admin';
 import MyMenu from './Menu';
 
-const MyLayout = (props) => <Layout {...props} menu={MyMenu} />;
+const MyLayout = (props) => <Layout {...props} menu={MyMenu}/>;
 
 export default MyLayout;
